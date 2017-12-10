c191f521b947cd1e86c82f2a23eeb23af8e3e962
Yongha Kwon <goh1211@naver.com>
2017-12-10 09:50:38 +0900

    Create writev_ex.c

---
+++ Linux/FILE_IO_2/writev_ex.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main(){
+  struct iovec iov[3];
+  ssize_t nr;
+  int fd, i;
+  
+  char *buf[] = { "term\n", "string\n", "hi\n"};
+  
+  fd = open("data.txt", O_WRONLY || O_CREAT || O_TRUNC);
+  
+  if(fd == -1){
+    perror("open");
+    return 1;
+ }
+ 
+ for( i = 0, i < 3 , i++){
+     iov[i].iov_base = buf[i];
+     iov[i].iov_len = strlen(buf[i]+1);
+ }
+ 
+ nr = writev(fd, iov,3 );
+ 
+ if( nr == -1){
+  perror("writev");
+  return 1;
+ }
+ printf("wrote %d bytes\n", nr);
+ 
+ if(close(fd)){
+   perror("close");
+   return 1 ;
+ }
+ 
+ return 0;
+}
+  
+  
+  
