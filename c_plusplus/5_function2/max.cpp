ab573877d1fa97dd862ed0ec1862df66d034cb77
Yongha Kwon <goh1211@naver.com>
2018-03-26 10:47:25 +0000

    Update max.cpp

--- c_plusplus/5_function2/max.cpp
+++ c_plusplus/5_function2/max.cpp
@@ -9,8 +9,8 @@ int main()
 	int ints[5] = { 123, 431, 231, 560, 323 };
 	double dbls[5] = { 3.71, 5.84, 37.8, 5.59, 2.11 };
 
-	cout << "가장 큰 int 값: " << max5(ints, 5) << endl;
-	cout << "가장 큰 double 값: " << max5(dbls, 5) << endl;
+	cout << max5(ints, 5) << endl;
+	cout << max5(dbls, 5) << endl;
 	return 0;
 }
 
